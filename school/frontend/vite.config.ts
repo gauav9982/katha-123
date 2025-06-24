f6a7f609bdc726e566783de06837670373649c80
Katha Super <katha@example.com>
2025-06-24 23:49:41 +0530

    Auto-commit before deployment

--- school/frontend/vite.config.ts
+++ school/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/school-app/',
   plugins: [react()],
   server: {
     port: 5179, // School frontend runs on this port
